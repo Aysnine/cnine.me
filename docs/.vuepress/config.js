48f0cf9759949e525c61bfae843c6bb74696aa91
CNine <cnine229@gmail.com>
2021-02-12 20:49:20 +0800

    chore: setup theme-blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,36 @@
 module.exports = {
   title: 'cnine.me',
-  description: 'personal website'
+  description: 'personal website',
+  theme: '@vuepress/blog',
+  themeConfig: {
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+    ],
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/Aysnine',
+        },
+      ],
+      copyright: [
+        {
+          text: 'Copyright © 2021-present cnine229@gmail.com',
+        },
+      ],
+    },
+    sitemap: {
+      hostname: 'https://cnine.me'
+    },
+    feed: {
+      canonical_base:'http://cnine.me',
+    },
+  },
 }
